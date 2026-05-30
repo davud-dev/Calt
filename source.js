c0136475c4dfe43cdc14696697ca923a1ed34a9e
davud-dev <cazamirloucas+calt@gmail.com>
2026-05-30 22:20:23 +0300

    Refactor lexer to include braces and operators

--- source.js
+++ source.js
@@ -1,7 +1,8 @@
 function CALT() {
     function lexer(src) {
-        let simples = {'(': "LPAREN", ')': "RPAREN", ',': "COMMA", '!': "OUTPUT_START", '?': "OUTPUT_END"};
-        let keywords = {"output": "OUTPUT", "org": "ORIGIN", "var": "VARIABLE", "const": "CONSTANT", "ret": "RETURN"};
+        let simples = {'(': "LPAREN", ')': "RPAREN", '{': "LBRACE", '}': "RBRACE", ',': "COMMA", "<": "OUTPUT_START", ">": "OUTPUT_END"};
+        let keywords = {"output": "START", "org": "ORIGIN", "var": "VARIABLE", "const": "CONSTANT", "ret": "RETURN"};
+        let ar_operators = {"+": "PLUS", "-": "MINUS", "*": "TIMES", "/": "DIVIDE", "%": "MODULO", "^": "POWER", "'": "SQUARE_ROOT", "!": "FACTORIAL", "?": "TERMIAL"};
         let pos = 0;
         let tokens = [];
         let line = 1;
@@ -23,20 +24,26 @@ function CALT() {
             char = src[pos];
             switch(true) {
                 case digit(char):
-                    val = '';
-                    while(digit(src[pos]) {
-                        val += src[pos];
-                        move();
+                    val = ''; // here for storing a solid value which can be used for our numbers "rendering" as a token
+                    while(digit(src[pos]) { // here for handling of regular integers
+                        val += src[pos]; 
+                        move(); 
                     };
-                    if(src[pos] === ".") {
+                    if(src[pos] === ".") { // here for handling of floats and what-not
                         while(digit(src[pos]) {
                             val += src[pos];
                             move();
                         };
                     };
-                    token("DIGIT", val);
+                    token("DIGIT", val); // here for pushing our token to the lexer
                     break;
-                case char in simples:
+                case char in simples: // here to simplify(ha, get it?) handling of single-character tokens
+                    let type = simples[char];
+                    token(type, char);
+                    break;
+                case char in keywords: // here for simplifying handling of different characters
+                    let type = keywords[char];
+                    token(type + "_KEYWORD", char);
                     break;
             };
       };    
